946e8063f352c7f500a466dc5441879d32bb4f6f
vm799 <89162649+vm799@users.noreply.github.com>
2022-02-06 20:07:29 +0000

    transitions

--- index.js
+++ index.js
@@ -22,3 +22,21 @@ btnHamburger.addEventListener("click", function(){
 
 }
 })
+
+function reveal(){
+     const reveals = document.querySelectorAll(".reveal")
+
+     for(let i=0; i < reveals.length; i++){
+          let windowHeight = window.innerHeight
+          let elementTop = reveals[i].getBoundingClientRect().top
+          let elementVisible = 150
+
+          if(elementTop < windowHeight - elementVisible){
+               reveals[i].classList.add("active")
+          } else {
+               reveals[i].classList.remove("active")
+          }
+     }
+}
+
+window.addEventListener("scroll", reveal)
